a2adbd0f669a13e9cea85a30c23fe18e411c5c30
Ryan Hinds <115953645+FossRyanH@users.noreply.github.com>
2023-04-17 11:19:40 -0600

    fixed gameover and win screens

--- godot-final-game-start-project/interface/WinGame.gd
+++ godot-final-game-start-project/interface/WinGame.gd
@@ -1,1 +1,19 @@
 extends Control
+
+onready var restart_button := $CenterContainer/VBoxContainer/RestartButton
+onready var quit_button := $CenterContainer/VBoxContainer/QuitButton
+
+var game_level = "res://rooms/GameLevel.tscn"
+
+
+func _ready() -> void:
+	restart_button.connect("button_down", self, "on_Restart_button_clicked")
+	quit_button.connect("button_down", self, "on_Quit_button_clicked")
+
+
+func on_Restart_button_clicked() -> void:
+	get_tree().change_scene(game_level)
+
+
+func on_Quit_button_clicked() -> void:
+	get_tree().quit()
